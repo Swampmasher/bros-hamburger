e193b2384cc56ee5263ff1d1e10ddde3f3a0a857
Swampmasher <76541139+Swampmasher@users.noreply.github.com>
2021-03-06 14:52:40 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.MinParaMiktar = 25
+Config.MaxParaMiktar = 225
+
+Config.BurgerEtPisirme = {x = -1202.08, y = -897.198, z = 13.995}
+Config.BurgerHazirlama = {x = -1197.00, y = -899.851, z = 13.995}
+Config.BurgerPaketleme = {x = -1197.97, y = -896.835, z = 13.995}
+Config.BurgerSatisyeri = {x = -1192.63, y = -894.986, z = 13.995}
+Config.Malzeme = {x = -1203.00, y = -895.390, z = 13.995}
+Config.PatatesPisir = {x = -1200.90, y = -898.958, z = 13.995}
+
